dfc990d503c326ce7f817c16c815a01919cfdc6a
YoctoHan <YoctoInch@gmail.com>
2025-05-24 14:47:56 +0800

    [新增特性] 参照最新的 README 修改了部分文件结构, 后续修改将不再改动原始文件, 只进行增量.

--- evaluations/eval_aixcoder_debug.py
+++ evaluations/eval_aixcoder_debug.py
@@ -1,12 +1,12 @@
 from mmengine.config import read_base
 
 with read_base():
-    from ...opencompass.configs.datasets.gsm8k.gsm8k_gen_aixcoder import \
+    from ..opencompass.configs.datasets.gsm8k.gsm8k_gen_aixcoder import \
         gsm8k_datasets
-    from ...opencompass.configs.datasets.math.math_0shot_gen_aixcoder import \
+    from ..opencompass.configs.datasets.math.math_0shot_gen_aixcoder import \
         math_datasets
 
-    from ...opencompass.configs.models.aixcoder.aixcoder_eval_debug import \
+    from ..opencompass.configs.models.aixcoder.aixcoder_eval_debug import \
         models as aixcoder
 
 datasets = gsm8k_datasets + math_datasets
--- evaluations/eval_aixcoder_eval_2.py
+++ evaluations/eval_aixcoder_eval_2.py
@@ -1,16 +1,16 @@
 from mmengine.config import read_base
 
 with read_base():
-    from ...opencompass.configs.datasets.gsm8k.gsm8k_gen_aixcoder import \
+    from ..opencompass.configs.datasets.gsm8k.gsm8k_gen_aixcoder import \
         gsm8k_datasets
-    from ...opencompass.configs.datasets.math.math_0shot_gen_aixcoder import \
+    from ..opencompass.configs.datasets.math.math_0shot_gen_aixcoder import \
         math_datasets
-    from ...opencompass.configs.datasets.mmlu.mmlu_gen_aixcoder import \
+    from ..opencompass.configs.datasets.mmlu.mmlu_gen_aixcoder import \
         mmlu_datasets
-    from ...opencompass.configs.datasets.ceval.ceval_gen_aixcoder import \
+    from ..opencompass.configs.datasets.ceval.ceval_gen_aixcoder import \
         ceval_datasets
 
-    from ...opencompass.configs.models.aixcoder.aixcoder_eval_2 import \
+    from ..opencompass.configs.models.aixcoder.aixcoder_eval_2 import \
         models as aixcoder
 
 datasets = gsm8k_datasets + math_datasets + mmlu_datasets + ceval_datasets
