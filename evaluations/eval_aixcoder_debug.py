09f1bcca3f28c7436433cf4d72f11d59265c977d
YoctoHan <YoctoInch@gmail.com>
2025-05-25 16:48:29 +0800

    [新增特性] 新增 LCB_datasets 评测.

--- evaluations/eval_aixcoder_debug.py
+++ evaluations/eval_aixcoder_debug.py
@@ -1,13 +1,11 @@
 from mmengine.config import read_base
 
 with read_base():
-    from ..opencompass.configs.datasets.demo.demo_gsm8k_base_gen import \
-        gsm8k_datasets
-    from ..opencompass.configs.datasets.demo.demo_math_base_gen import \
-        math_datasets
+    from ..opencompass.configs.datasets.livecodebench.livecodebench_gen_aixcoder import \
+        LCB_datasets
 
     from ..opencompass.configs.models.aixcoder.aixcoder_eval_debug import \
         models as aixcoder
 
-datasets = gsm8k_datasets + math_datasets
+datasets = LCB_datasets
 models = aixcoder
