c7d379a1fe5a123aa95dcd9768d35c1401180ca5
YoctoHan <YoctoInch@gmail.com>
2025-05-25 12:45:53 +0800

    [BUG修复] 定位到了问题开始解决, 先保存初始状态.

--- evaluations/eval_aixcoder_debug.py
+++ evaluations/eval_aixcoder_debug.py
@@ -1,9 +1,9 @@
 from mmengine.config import read_base
 
 with read_base():
-    from ..opencompass.configs.datasets.gsm8k.gsm8k_gen_aixcoder import \
+    from ..opencompass.configs.datasets.demo.demo_gsm8k_base_gen import \
         gsm8k_datasets
-    from ..opencompass.configs.datasets.math.math_0shot_gen_aixcoder import \
+    from ..opencompass.configs.datasets.demo.demo_math_base_gen import \
         math_datasets
 
     from ..opencompass.configs.models.aixcoder.aixcoder_eval_debug import \
